e38a10cfc8c7a598103aa3123ea962692ff4268e
shubhXYZ <shbhmsingh039@gmail.com>
2025-03-08 14:34:26 +0530

    create docker file for backend

--- backend/app/models.py
+++ backend/app/models.py
@@ -22,11 +22,11 @@ class Leads(BaseDBModel):
     __tablename__ = "leads"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    createdAt = Column(DateTime, default=func.now())
-    updatedAt = Column(DateTime, default=func.now(), onupdate=func.now())
+    createdAt = Column("createdat", DateTime, default=func.now())
+    updatedAt = Column("updatedat", DateTime, default=func.now(), onupdate=func.now())
     name = Column(String)
     email = Column(String, unique=True, index=True)
     company = Column(String)
     engaged = Column(Boolean)
     stage = Column(Integer, default=0)
-    lastContacted = Column(Date, nullable=True)
+    lastContacted = Column("lastcontacted", Date, nullable=True)
